f32b033b5c90e5cd6a1b04955cfde2e025481ee9
José Bello <jfbello.work@gmail.com>
2025-03-13 20:30:39 +0100

    Ch5- Add flask.request, debug=True, POST method and retrieve variables from form

--- hello_flask.py
+++ hello_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -9,9 +9,11 @@ def hello() -> str:
     return 'Hello world from Flask!'
 
 
-@app.route('/search4')
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
-    return str(search4letters('life, the universe and everything', 'eiru,!'))
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    return str(search4letters(phrase, letters))
 
 
 @app.route('/entry')
@@ -19,4 +21,4 @@ def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to the search4letters on the web!')
 
 
-app.run()
+app.run(debug=True)
