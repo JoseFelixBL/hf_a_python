fc28e4dfc08f2c6f6b013da5a73b2f6358327ff4
José Bello <jfbello.work@gmail.com>
2025-03-17 08:48:28 +0100

    Ch6- logeando cada registro en una línea

--- vsearchweb.py
+++ vsearchweb.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, res, file = log)
+        print(req.form, file = log, end = '|')
+        print(req.remote_addr, file = log, end = '|')
+        print(req.user_agent, file = log, end = '|')
+        print(res, file = log)
 
 
 @app.route('/search4', methods=['POST'])
