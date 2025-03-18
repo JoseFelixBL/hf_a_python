28e1a99aa8ef5a3872eb56678fe495ffcb036313
José Bello <jfbello.work@gmail.com>
2025-03-18 12:06:13 +0100

    Agregar directorio var/ para guardar los logs en BIND_MOUNT y tenerlos disponibles desde el host y no perderlos entre run y run

--- vsearchweb.py
+++ vsearchweb.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request
 from markupsafe import escape
 from vsearch import search4letters
+from pathlib import Path
 
 app = Flask(__name__)
 
+log_dir = Path('var')
+log_name = Path('vsearch.log')
+log_file = log_dir / log_name
+
+
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file = log, sep = '|')
+    with open(log_file, 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -25,11 +31,13 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to the search4letters on the web!')
 
+
 @app.route('/viewlog')
 def view_the_log() -> str:
-    with open('vsearch.log') as log:
+    with open(log_file) as log:
         contents = log.read()
     return escape(contents)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
