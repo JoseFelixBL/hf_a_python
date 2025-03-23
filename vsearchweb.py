9f3d2f4e2d3b514c1392818ab83007d42e407e02
José Bello <jfbello.work@gmail.com>
2025-03-23 23:55:25 +0100

    Ch9- Modificar vsearchweb.py  log_request y view_the_log para usar DBcm.py y usar mariadb

--- vsearchweb.py
+++ vsearchweb.py
@@ -2,45 +2,32 @@ from flask import Flask, render_template, request
 from markupsafe import escape
 from vsearch import search4letters
 from pathlib import Path
-import mariadb
+from DBcm import UseDatabase
 
 app = Flask(__name__)
 
-log_dir = Path('var')
-log_name = Path('vsearch.log')
-log_file = log_dir / log_name
+# app.config['log_dir'] = Path('var')
+# app.config['log_name'] = Path('vsearch.log')
+# app.config['log_file'] = app.config['log_dir'] / app.config['log_name']
+
+app.config['dbconfig'] = {'host': '127.0.0.1',
+                          'user': 'vsearch',
+                          'password': 'vsearchpasswd',
+                          'database': 'vsearchlogDB', }
 
 
 def log_request(req: 'flask_request', res: str) -> None:
     """Log details of the web request and the results."""
-    # Lo anterior:
-    # with open(log_file, 'a') as log:
-    #     print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
-
-    # Solo req.user_agent.string tiene contenido.
-    # print(dir(req))
-    # print(dir(req.user_agent))
-    # print(f'{req.user_agent.browser=}', f'{req.user_agent.language=}', f'{req.user_agent.platform=}',
-    #       f'{req.user_agent.string=}', f'{req.user_agent.to_header=}', f'{req.user_agent.version=}')
-
-    dbconfig = {'host': '127.0.0.1',
-                'user': 'vsearch',
-                'password': 'vsearchpasswd',
-                'database': 'vsearchlogDB', }
-    conn = mariadb.connect(**dbconfig)
-    cursor = conn.cursor()
-    _SQL = """insert into log  
-              (phrase, letters, ip, browser_string, results) 
-              values 
-              (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form['phrase'],
-                          req.form['letters'],
-                          req.remote_addr,
-                          req.user_agent.string,
-                          res, ))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log  
+                (phrase, letters, ip, browser_string, results) 
+                values 
+                (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                              req.form['letters'],
+                              req.remote_addr,
+                              req.user_agent.string,
+                              res, ))
 
 
 @app.route('/search4', methods=['POST'])
@@ -61,16 +48,14 @@ def entry_page() -> 'html':
 
 
 @app.route('/viewlog')
-def view_the_log() -> str:
-    content = []
-    with open(log_file) as log:
-        for line in log:
-            content.append([])
-            for item in line.split('|'):
-                content[-1].append(escape(item))
-
+def view_the_log() -> 'html':
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results
+                from log"""
+        cursor.execute(_SQL)
+        content = cursor.fetchall()
     title = 'Log View'
-    row_titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    row_titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
     return render_template('viewlog.html', the_title=title, the_row_titles=row_titles, the_data=content)
 
 
