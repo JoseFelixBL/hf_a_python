c219f2db283f76b92bf2d11642558567e3e5eb17
José Bello <jfbello.work@gmail.com>
2025-03-24 23:11:44 +0100

    Ch10- Agregar session a vsearchweb.py para ver log solo si se han logeado, agregar vslog_checker.py - decorador

--- vsearchweb.py
+++ vsearchweb.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from markupsafe import escape
 from vsearch import search4letters
 from DBcm import UseDatabase
 import os
+from checker import check_logged_in
+
 
 app = Flask(__name__)
+app.secret_key = 'YouWillNeverGuessMySecretKey'
 
 HOST = os.getenv('DB_HOST')
 
@@ -46,6 +49,7 @@ def entry_page() -> 'html':
 
 
 @app.route('/viewlog')
+@check_logged_in
 def view_the_log() -> 'html':
     with UseDatabase(app.config['dbconfig']) as cursor:
         _SQL = """select phrase, letters, ip, browser_string, results
@@ -55,6 +59,26 @@ def view_the_log() -> 'html':
     title = 'Log View'
     row_titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
     return render_template('viewlog.html', the_title=title, the_row_titles=row_titles, the_data=content)
+
+
+@app.route('/login')
+def login() -> 'str':
+    session['logged_in'] = True
+    return 'You are now logged in'
+
+
+@app.route('/logout')
+def logout() -> 'str':
+    if 'logged_in' in session:
+        session.pop('logged_in')
+    return 'You are now logged OUT'
+
+
+@app.route('/log_status')
+def log_status() -> 'str':
+    if 'logged_in' in session:
+        return 'You are logged in'
+    return 'You are NOT logged in'
 
 
 if __name__ == '__main__':
---
+++ vslog_checker.py
@@ -0,0 +1,17 @@
+from flask import session
+from functools import wraps
+
+
+# New decorator function receives a function as argument
+def check_logged_in(func):
+    # Wraps decorator needed! always!
+    @wraps(func)
+    # We define a 'wrapper' function that receives the arguments of the wrapped function
+    def wrapper(*args, **kwargs):
+        # Inside 'wrapper' we do the new logic and
+        # we finish calling the wrapped function to do it function
+        if 'logged_in' in session:
+            return func(*args, **kwargs)
+        return 'You are NOT logged in.'
+    # At the end of the decorator we return the 'wrapper' function
+    return wrapper
