ac6933353df60e7f58e2c697bf964acf0fa4ffa9
José Bello <jfbello.work@gmail.com>
2025-03-24 19:03:25 +0100

    Ch10- simple_webapp.py with checks in every page

--- simple_webapp.py
+++ simple_webapp.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 app.secretkey = 'YouWillNeverGuessMySecretKey'
 
 
+def check_logged_in() -> bool:
+    if 'logged_in' in session:
+        return True
+    return False
+
+
 @app.route('/')
 def hello() -> str:
     return 'Hello from the simple webapp.'
@@ -11,16 +17,22 @@ def hello() -> str:
 
 @app.route('/page1')
 def page1() -> str:
+    if not check_logged_in():
+        return 'You are NOT logged in.'
     return 'This is page 1.'
 
 
 @app.route('/page2')
 def page2() -> str:
+    if not check_logged_in():
+        return 'You are NOT logged in.'
     return 'This is page 2.'
 
 
 @app.route('/page3')
 def page3() -> str:
+    if not check_logged_in():
+        return 'You are NOT logged in.'
     return 'This is page 3.'
 
 
@@ -31,9 +43,16 @@ def login() -> 'str':
 
 
 @app.route('/logout')
-def login() -> 'str':
-    session['logged_in'].clear()
+def logout() -> 'str':
+    session.pop('logged_in')
     return 'You are now logged OUT'
+
+
+@app.route('/status')
+def status() -> 'str':
+    if 'logged_in' in session:
+        return 'You are now logged in'
+    return 'You are NOT logged in'
 
 
 if __name__ == '__main__':
