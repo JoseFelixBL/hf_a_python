67d42044ddb75c82a3b9a398b113109c72782047
José Bello <jfbello.work@gmail.com>
2025-03-24 20:17:24 +0100

    Ch10- ejemplo_funciones.py Argumentos en funciones, listas y diccionarios

--- ejemplo_funciones.py
+++ ejemplo_funciones.py
@@ -43,3 +43,66 @@ i = outer()
 print(f'{id(i)=}, {type(i)=}')
 print('i()')
 print(f'{i()}')
+
+print(f'{"-" * 30}')
+
+# Procesando una lista de argumentos
+
+
+def my_func(*args):
+    for a in args:
+        print(a, end=' ')
+    if args:
+        print()
+
+
+my_func(10)
+my_func()
+my_func(10, 20, 30, 40, 50)
+my_func(1, 'dos', 3, 'cuatro', 5)
+
+values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'diez']
+my_func(values)
+my_func(*values)
+
+print(f'{"-" * 30}')
+
+# Procesando diccionarios
+
+
+def my_dict_func(**kwargs):
+    for k, v in kwargs.items():
+        print(k, v, sep='->', end=' ')
+    if kwargs:
+        print()
+
+
+my_dict_func(a=10, b=20)
+my_dict_func()
+my_dict_func(a=10, b=20, c=30, d=40, e=50)
+
+dbconfig = {'host': '127.0.0.1',
+            'user': 'vsearch',
+            'password': 'vsearchpasswd',
+            'database': 'vsearchlogDB', }
+my_dict_func(**dbconfig)
+
+# Procesando cualquier número y tipo de argumentos de función
+
+
+def my_func_3(*args, **kwargs):
+    for a in args:
+        print(a, end=' ')
+    # if args:
+    #     print()
+
+    for k, v in kwargs.items():
+        print(k, v, sep='->', end=' ')
+    # if kwargs:
+    print()
+
+
+my_func_3(1, 2, 3)
+my_func_3()
+my_func_3(a=10, b=20, c=30)
+my_func_3(1, 2, 3, a=10, b=20, c=30)
